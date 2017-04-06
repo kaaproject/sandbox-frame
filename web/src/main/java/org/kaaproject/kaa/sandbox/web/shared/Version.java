30d338e074b0138de4cbb9d9e57682cd45a19bf7
Anatoly Yarinich <anatoly.yarinich@gmail.com>
2017-04-06 18:22:45 +0300

    KSF-117: Set Kaa and Sample Apps dependency versions

--- web/src/main/java/org/kaaproject/kaa/sandbox/web/shared/Version.java
+++ web/src/main/java/org/kaaproject/kaa/sandbox/web/shared/Version.java
@@ -27,13 +27,13 @@ package org.kaaproject.kaa.sandbox.web.shared;
 public final class Version {
 
     /** The Constant PROJECT_VERSION. */
-    public static final String PROJECT_VERSION = "1.5.0-SNAPSHOT";
+    public static final String PROJECT_VERSION = "1.5.0";
     
     /** The Constant KAA_VERSION. */
-    public static final String KAA_VERSION = "0.11.0-SNAPSHOT";
+    public static final String KAA_VERSION = "0.10.1";
 
     /** The Constant KAA_SAMPLE_APPS_VERSION. */
-    public static final String KAA_SAMPLE_APPS_VERSION = "2.1.0-SNAPSHOT";
+    public static final String KAA_SAMPLE_APPS_VERSION = "2.1.0";
 
     /**
      * Instantiates a new version.
